b01ef28b958980a258de5d4ac35c31f3ff5d4d68
Mace <maceplus@gmail.com>
2018-10-11 00:05:51 +0100

    webpack install

--- index.js
+++ index.js
@@ -1,41 +1,31 @@
 //---------- VUE --------------
 
-new Vue({
+const repo = new Vue({
   el: '#vue-repos',
   data: {
     title: 'Vue.js repositories',
-    repos: vueProject
+    repos: []
   },
 });
 
-
 //------- Ajax --------------------
-
-const vueProject =
-  fetch(gitHubURL(vuejs))
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error('Bad Response')
-      }
-    })
-    .then(getRepos)
-    .then((data) => {
-      return data
-    })
-    .catch((error) => {
-      console.error(error)
-    });
+fetch(gitHubURL('vuejs'))
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Bad Response')
+    }
+  })
+  .then((data) => {
+    return repo.repos = data
+  })
+  .catch((error) => {
+    console.error(error)
+  });
 
 //------- Functions -----------
 
 function gitHubURL(gitUser) {
-  return `https://api.github.com/users/${gitUser}`
+  return `https://api.github.com/users/${gitUser}/repos`
 };
-
-function getRepos(repo) {
-  return repo.data;
-}
-
-
